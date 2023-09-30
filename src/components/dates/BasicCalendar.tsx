a644530bf0a219da28ca7947f5855f61256f7c61
MasanoriOno <masanori.ono@arsaga.jp>
2023-10-01 00:27:44 +0900

    ログ上での日付複数選択はできたが、選択された日付の背景色変更がうまくいかないので一旦中断。

--- src/components/dates/BasicCalendar.tsx
+++ src/components/dates/BasicCalendar.tsx
@@ -1,15 +1,38 @@
-import { DateCalendar } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import ja from "date-fns/locale/ja";
+import { useState } from "react";
+import { DatePicker } from "@mui/x-date-pickers";
+import { styled } from "@mui/material/styles";
+import { PickersDay } from "@mui/x-date-pickers";
 
 export const BasicCalendar = () => {
+  const [selectedDates, setSelectedDates] = useState<Date[]>([]);
+
+  const handleDateChange = (newDate) => {
+    // すでに選択されている日付であればリストから削除、そうでなければ追加
+    if (
+      selectedDates.some((date) => date.toISOString() === newDate.toISOString())
+    ) {
+      setSelectedDates((prev) =>
+        prev.filter((date) => date.toISOString() !== newDate.toISOString()),
+      );
+    } else {
+      setSelectedDates((prev) => [...prev, newDate]);
+    }
+
+    console.log(selectedDates);
+  };
+
+  const CustomDatePickerDay = styled(PickersDay)(() => ({
+    backgroundColor: "lightblue",
+  }));
+
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ja}>
-      <DateCalendar
-        onChange={() => {
-          console.log("Date changed");
-        }}
+      <DatePicker
+        onChange={handleDateChange}
+        slots={{ day: CustomDatePickerDay }}
       />
     </LocalizationProvider>
   );
